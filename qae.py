50639e6de900f8b8a09b23a42d45dd5389059720
Jeong Ho Chu <jhchu@hotmail.com>
2025-09-01 16:36:18 +0900

    QAE 추가

--- qae.py
+++ qae.py
@@ -57,19 +57,65 @@ ae = AmplitudeEstimation(
 ae_result = ae.estimate(problem)
 print(ae_result.estimation)
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-# plot estimated values
-gridpoints = list(ae_result.samples.keys())
-probabilities = list(ae_result.samples.values())
+# # plot estimated values
+# gridpoints = list(ae_result.samples.keys())
+# probabilities = list(ae_result.samples.values())
 
-plt.bar(gridpoints, probabilities, width=0.5 / len(probabilities))
-plt.axvline(p, color="r", ls="--")
-plt.xticks(size=15)
-plt.yticks([0, 0.25, 0.5, 0.75, 1], size=15)
-plt.title("Estimated Values", size=15)
-plt.ylabel("Probability", size=15)
-plt.xlabel(r"Amplitude $a$", size=15)
-plt.ylim((0, 1))
-plt.grid()
-plt.show()
+# plt.bar(gridpoints, probabilities, width=0.5 / len(probabilities))
+# plt.axvline(p, color="r", ls="--")
+# plt.xticks(size=15)
+# plt.yticks([0, 0.25, 0.5, 0.75, 1], size=15)
+# plt.title("Estimated Values", size=15)
+# plt.ylabel("Probability", size=15)
+# plt.xlabel(r"Amplitude $a$", size=15)
+# plt.ylim((0, 1))
+# plt.grid()
+# plt.show()
+
+print("Interpolated MLE estimator:", ae_result.mle)
+# ae_circuit = ae.construct_circuit(problem)
+# ae_circuit.decompose().draw(
+#     "mpl", style="clifford"
+# )  # decompose 1 level: exposes the Phase estimation circuit!
+
+# from qiskit import transpile
+# basis_gates = ["h", "ry", "cry", "cx", "ccx", "p", "cp", "x", "s", "sdg", "y", "t", "cz"]
+# transpile(ae_circuit, basis_gates=basis_gates, optimization_level=2).draw("mpl", style="clifford")
+
+
+from qiskit_algorithms import IterativeAmplitudeEstimation
+
+iae = IterativeAmplitudeEstimation(
+    epsilon_target=0.01,  # target accuracy
+    alpha=0.05,  # width of the confidence interval
+    sampler=sampler,
+)
+iae_result = iae.estimate(problem)
+
+print("Estimate:", iae_result.estimation)
+
+# iae_circuit = iae.construct_circuit(problem, k=3)
+# iae_circuit.draw("mpl", style="clifford")
+
+from qiskit_algorithms import MaximumLikelihoodAmplitudeEstimation
+
+mlae = MaximumLikelihoodAmplitudeEstimation(
+    evaluation_schedule=3,  # log2 of the maximal Grover power
+    sampler=sampler,
+)
+mlae_result = mlae.estimate(problem)
+
+print("Estimate:", mlae_result.estimation)
+
+from qiskit_algorithms import FasterAmplitudeEstimation
+
+fae = FasterAmplitudeEstimation(
+    delta=0.01,  # target accuracy
+    maxiter=3,  # determines the maximal power of the Grover operator
+    sampler=sampler,
+)
+fae_result = fae.estimate(problem)
+
+print("Estimate:", fae_result.estimation)
